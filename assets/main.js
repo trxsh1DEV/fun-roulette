25741c0ac3c37a0bad5d7b34711c24afe68d3e88
trxsh1 <yagosousa630@gmail.com>
2023-03-24 13:45:43 -0300

    cleaning up code

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,7 @@
 const wheel = document.querySelector('.wheel');
 const spinBtn = document.querySelector('.spin-btn');
-// let valueRotation = Math.round((Math.random() * 3000));
-// let valueRotation = Math.floor(Math.random() * (360 - 1) + 1);
 const rand = (max, min) => Math.floor(Math.random() * (max - min) + min) * 9;
-let valueRotation = rand(360, 45);
+let valueRotation = rand(360, 45) + 360;
 const pointer = document.querySelector('.selector');
 const t = document.querySelectorAll('.numb');
 // let currentRotate = 0;
@@ -13,10 +11,10 @@ spinBtn.addEventListener('click', () => {
     // wheel.style.transform = "rotate("+ valueRotation + "deg)";
     wheel.style.transform = `rotate(${valueRotation}deg)`;
     // currentRotate += valueRotation;
-    console.log('a', valueRotation);
-    console.log('rotação atual','rotação roleta', valueRotation);
-    let convertRotate = valueRotation / 45;
-    console.log('/ 45', convertRotate,'casas puladas', Math.round(convertRotate));
+    // console.log('a', valueRotation);
+    // console.log('rotação atual','rotação roleta', valueRotation);
+    let convertRotate = Math.round(valueRotation / 45);
+    console.log('casas puladas', convertRotate);
     valueRotation += rand(720, 405);
     // console.log(t[0].firstElementChild.textContent);
     // if(currentRotate <= 90){
@@ -24,8 +22,4 @@ spinBtn.addEventListener('click', () => {
     //     // console.log(t[0].firstElementChild.textContent);
     //     return
     // }
-    // const rand = (max, min) => Math.floor(Math.random() * (max - min) + min);
-    // const rand = Math.floor(Math.random() * (360 - 1) + 1);
-    // console.log(rand);
-    // console.log(valueRotation);
-})
+});

1aed0e84a5da946271d7319848f9e4ef17cefbc3
trxsh1 <yagosousa630@gmail.com>
2023-03-24 13:20:14 -0300

    roulette angle adjustments

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,31 @@
-let wheel = document.querySelector('.wheel');
-let spinBtn = document.querySelector('.spin-btn');
-let valueRotation = Math.round((Math.random() * 2000) + 500);
+const wheel = document.querySelector('.wheel');
+const spinBtn = document.querySelector('.spin-btn');
+// let valueRotation = Math.round((Math.random() * 3000));
+// let valueRotation = Math.floor(Math.random() * (360 - 1) + 1);
+const rand = (max, min) => Math.floor(Math.random() * (max - min) + min) * 9;
+let valueRotation = rand(360, 45);
+const pointer = document.querySelector('.selector');
+const t = document.querySelectorAll('.numb');
+// let currentRotate = 0;
 // let test = Math.ceil(Math.random() * 360);
 
 spinBtn.addEventListener('click', () => {
     // wheel.style.transform = "rotate("+ valueRotation + "deg)";
     wheel.style.transform = `rotate(${valueRotation}deg)`;
-    valueRotation += Math.round((Math.random() * 2000) + 500);
-    // console.log(wheel.style.transform);
+    // currentRotate += valueRotation;
+    console.log('a', valueRotation);
+    console.log('rotação atual','rotação roleta', valueRotation);
+    let convertRotate = valueRotation / 45;
+    console.log('/ 45', convertRotate,'casas puladas', Math.round(convertRotate));
+    valueRotation += rand(720, 405);
+    // console.log(t[0].firstElementChild.textContent);
+    // if(currentRotate <= 90){
+    //     // console.log(t[0].getAttribute('data-id').valueOf())
+    //     // console.log(t[0].firstElementChild.textContent);
+    //     return
+    // }
+    // const rand = (max, min) => Math.floor(Math.random() * (max - min) + min);
+    // const rand = Math.floor(Math.random() * (360 - 1) + 1);
+    // console.log(rand);
     // console.log(valueRotation);
 })
